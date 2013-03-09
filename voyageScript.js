829e39fd30ff5c9321ee3a497448222a2852f3b9
madhug <madhupramod@gmail.com>
2013-03-09 01:48:36 -0500

    add pages to demonstrate the actions

--- voyageScript.js
+++ voyageScript.js
@@ -4,14 +4,14 @@
 		var svg = d3.select("#chart").append("svg")
 		.attr("class","ui-grid-a .ui-responsive");
 		var ypos = 20
-		/*d3.json(
+		d3.json(
           'data.json',
           function(data)
 		  {
-			images = svg.selectall("image").data(data);
-			images.enter().append("image")
-			.attr("href",function(d){return d.image;});
-		  });*/
+			images = svg.selectall("circle").data(data);
+			images.enter().append("circle")
+			.attr("href",function(d){return d.identity[0].items;});
+		  });
 		});
 		$(function() {
 			$('div[data-role="dialog"]').live('pagebeforeshow', function(e, ui) {
